fe42fe3b22e2e02c7ea608933bbe7729768ea714
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2025-12-25 20:09:44 -0500

    Initial Commit

---
+++ dinastia/dinastiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  dinastiaApp.swift
+//  dinastia
+//
+//  Created by Paul Montealegre on 25/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct dinastiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
